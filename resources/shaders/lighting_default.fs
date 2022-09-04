cbfbb05e0d81de4812ce4e48e532afcbf174daea
kalluwa <kalluwa@hotmail.com>
2022-09-04 10:34:38 +0800

    merge heart effect

--- resources/shaders/lighting_default.fs
+++ resources/shaders/lighting_default.fs
@@ -99,31 +99,50 @@ float ShadowCalculation(vec3 fragPosWorldSpace)
     return shadow;
 }
 
-vec4 scPostW;
-// From http://fabiensanglard.net/shadowmappingVSM/index.php
-float chebyshevUpperBound(float distance)
+vec3 getDiffuse()
 {
-	vec2 moments = texture(shadowMap,vec3(scPostW.xy,0)).rg;
-	
-	// Surface is fully lit. as the current fragment is before the light occluder
-	if (distance <= moments.x)
-		return 1.0;
+    vec3 diffuse = texture(diffuse_map,vs_uv).xyz;
+    
+    vec3 col = vec3(1.0,0.9,0.7);
+        
+    vec3 worldpos = (inv_view * vec4(texture(position_map,vs_uv).xyz,1.0)).xyz;
+    vec3 worldnormal = texture(normal_map,vs_uv).xyz;
+    
+    vec2 p = (2.0*gl_FragCoord.xy-viewport_size.xy)/viewport_size.y;
 
-	// The fragment is either in shadow or penumbra. We now use chebyshev's upperBound to check
-	// How likely this pixel is to be lit (p_max)
-	float variance = moments.y - (moments.x*moments.x);
-	//variance = max(variance, 0.000002);
-	variance = max(variance, 0.00002);
+    {
+        vec3 rd = -normalize(cam_pos - worldpos);
+        vec3 pos = worldpos;
+        vec3 nor = normalize(worldnormal);
+        vec3 ref = reflect( rd, nor );
+        float fre= clamp( 1.0 + dot(nor,rd), 0.0, 1.0 );
+        	
+        float occ = 0.45;
+        	
+        col = vec3(0.9,0.02,0.01);
+        col = col*0.72 + 0.2*fre*vec3(1.0,0.8,0.2);
+        float max_n = max(nor.y,nor.y);
+        max_n = max(nor.y,max_n);
+        vec3 lin  = 4.0*vec3(0.7,0.80,1.00)*(0.5+0.5*max_n)*pow(occ,0.5);
+                lin += 0.8*fre*vec3(1.0,1.0,1.00)*(0.6+0.4*occ);
+        col = col * lin;
+        col += 2.5*vec3(0.8,0.9,1.00)*smoothstep(0.0,0.4,ref.y)*(0.06+0.94*pow(fre,5.0))*occ;
+    		
+    	col = pow(col,vec3(0.4545));
+    }
+    col = clamp(col,0.0,1.0);
+        
+    vec2 q = gl_FragCoord.xy/viewport_size.xy;
+    col *= 0.2 + 0.8*pow(16.0*q.x*q.y*(1.0-q.x)*(1.0-q.y),0.2);
 
-	float d = distance - moments.x;
-	float p_max = variance / (variance + d*d);
+    vec3 ambient = vec3(0.2f,0.2f,0.2f);
+    diffuse = diffuse * col;
 
-	return p_max;
+    return diffuse;
 }
-
 vec3 testShadow()
 {
-    vec3 color = texture(diffuse_map,vs_uv).xyz;// texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 color = getDiffuse()*1.3f;//texture(diffuse_map,vs_uv).xyz;// texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(texture(normal_map,vs_uv).xyz);
     vec3 lightColor = vec3(0.9);
     // ambient
@@ -164,21 +183,6 @@ void main()
 
     vec2 p = (2.0*gl_FragCoord.xy-viewport_size.xy)/viewport_size.y;
 
-    //shadow
-    vec4 sc = cameraToShadowProjector * vec4(worldpos,1.0);
-	scPostW = sc / sc.w; 
-	scPostW = scPostW * 0.5 + 0.5;
-
-    //color = vec3(scPostW.xy,0);
-    //color = texture(shadowMap,scPostW.xy).xyz;
-    //return;
-	float shadowFactor = 1.0; // Not in shadow
-    bool outsideShadowMap = sc.w <= 0.0f || (scPostW.x < 0 || scPostW.y < 0) || (scPostW.x >= 1 || scPostW.y >= 1);
-	if (!outsideShadowMap) 
-	{
-		shadowFactor = chebyshevUpperBound(scPostW.z);
-	}
-
     //vec3 col = render( p );
     {
         vec3 rd = -normalize(cam_pos - worldpos);
@@ -207,10 +211,6 @@ void main()
 
     vec3 ambient = vec3(0.2f,0.2f,0.2f);
     color = color * col;// * shadowFactor;
-
-    float shadow = ShadowCalculation(worldpos);                      
-    vec3 lighting = (vec3(0.3,0.3,0.3) + 0.7*(1.0 - shadow)) * color;    
-    color = lighting;
 
     //gamma fix
     //color = pow(color,vec3(1.0/2.2));
